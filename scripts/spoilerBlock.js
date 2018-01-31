65da16178ae11503b900ff1773860e6543e367d0
Viacheslav <viacheslavolshevskyi@gmail.com>
2018-01-31 12:28:15 +0200

    Task #4281 resolved

--- scripts/spoilerBlock.js
+++ scripts/spoilerBlock.js
@@ -6,9 +6,25 @@ function wrt(x)
     $(".razv").html(x);
 }
 
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function checkShowingBlockInCourse() {
+    var blockStatus = getCookie('displayBlock');
+    if(blockStatus=='false'){
+        $('.bgBlue').hide();
+    }
+}
+checkShowingBlockInCourse();
+
 function xexx()
 {
     $('.bgBlue').hide();
+    document.cookie = "displayBlock=false;expires=0;";
 }
 function courseTypeSpoiler(el) {
     if ($('#typeList').css('display')=='none') {
