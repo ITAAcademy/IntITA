d01966f6eb7c68bcdaca67b57f166032c2aa3a08
Liubov Z <kirlyubov@gmail.com>
2018-02-28 20:06:17 +0200

    Course Starts on mobile

--- protected/views/courses/_coursesPart1.php
+++ protected/views/courses/_coursesPart1.php
@@ -17,13 +17,16 @@
             <div class='courseBox'>
                 <div class="displayMini">
                     <img class="courseLogo" src='<?php echo StaticFilesHelper::createPath('image', 'course', $val[0]->course_img); ?>'>
+                    <div class='starLevelIndex'>
+                        <?php echo CommonHelper::getRating($val[0]->rating); ?>
+                    </div>
                     <div class='courseNameMini'><a
                             href="<?php echo Yii::app()->createUrl('course/index', array('id' => $val[0]->course_ID)); ?>"><?php
                             echo $val[0]->getTitle(); ?></a>
                     </div>
-                    <div class='starLevelIndex'>
-                        <?php echo CommonHelper::getRating($val[0]->rating); ?>
-                    </div>
+<!--                    <div class='starLevelIndex'>-->
+<!--                        --><?php //echo CommonHelper::getRating($val[0]->rating); ?>
+<!--                    </div>-->
                 </div>
                 <div class="courseInfo">
                     <div class='courseName'><a
