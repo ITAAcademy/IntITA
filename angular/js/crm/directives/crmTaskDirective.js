7129f6f95f71ff9c1718e13a804fab74f87fd261
Wizlight <wizlightdragon@gmail.com>
2018-03-02 15:57:21 +0200

    hot fix crm and instruction

--- angular/js/crm/directives/crmTaskDirective.js
+++ angular/js/crm/directives/crmTaskDirective.js
@@ -530,7 +530,15 @@ angular
                                 self.data.executant = null;
                             }
                         }
-                    }
+                    },
+                    isObserver: function (user) {
+                        var observer = _.find(self.data.roles['observer'], {id: String(user)});
+                        if(_.isObject(observer)){
+                            return true;
+                        }else{
+                            return false;
+                        }
+                    },
                 };
 
                 crmTaskServices
