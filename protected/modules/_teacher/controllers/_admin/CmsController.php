050bafd4dfb31461fca9463690d56d5663274a91
Wizlight <wizlightdragon@gmail.com>
2018-03-10 11:38:53 +0200

    cms header

--- protected/modules/_teacher/controllers/_admin/CmsController.php
+++ protected/modules/_teacher/controllers/_admin/CmsController.php
@@ -16,4 +16,51 @@ class CmsController extends TeacherCabinetController{
     {
         $this->renderPartial('index', array(),false,true);
     }
+
+    public function actionMenuLists()
+    {
+        $this->renderPartial('list/lists');
+    }
+
+    public function actionGetMenuList()
+    {
+       echo CJSON::encode(CmsMenuList::model()->findAll());
+    }
+
+    public function actionUpdateMenuLink()
+    {
+        $result = ['message' => 'OK'];
+        $statusCode = 201;
+
+        try {
+            $params = array_filter((array)json_decode($_POST['data']));
+            $menuLink = isset($params['id'])?CmsMenuList::model()->findByPk($params['id']): new CmsMenuList();
+            $menuLink->id_organization = Yii::app()->user->model->getCurrentOrganizationId();
+            $menuLink->attributes = $params;
+            if(!$menuLink->save()){
+                throw new \application\components\Exceptions\IntItaException(500, $menuLink->getValidationErrors());
+            }
+        } catch (Exception $error) {
+            $statusCode = 500;
+            $result = ['message' => 'error', 'reason' => $error->getMessage()];
+        }
+
+        $this->renderPartial('//ajax/json', ['statusCode' => $statusCode, 'body' => json_encode($result)]);
+    }
+
+    public function actionRemoveMenuLink()
+    {
+        $result = ['message' => 'OK'];
+        $statusCode = 201;
+
+        try {
+            $menuLink = CmsMenuList::model()->findByPk($_POST['id']);
+            $menuLink->delete();
+
+        } catch (Exception $error) {
+            $statusCode = 500;
+            $result = ['message' => 'error', 'reason' => $error->getMessage()];
+        }
+        $this->renderPartial('//ajax/json', ['statusCode' => $statusCode, 'body' => json_encode($result)]);
+    }
 }
