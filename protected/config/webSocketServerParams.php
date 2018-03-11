1ea2e347c8eed3058c6cc6e7c1f4fd6c12b6b15e
nick <nick.zorych@gmail.com>
2018-03-11 17:53:01 +0200

    add composer container

--- protected/config/webSocketServerParams.php
+++ protected/config/webSocketServerParams.php
@@ -7,7 +7,7 @@
  */
 return [
     'useWebSocketNotification'=>true,
-    'address'=>'127.0.0.1',
+    'address'=>'172.17.0.5',
     'port'=>'9500',
     'zmqServerAddress'=>'127.0.0.1',
     'zmqServerPort'=>'8990',
