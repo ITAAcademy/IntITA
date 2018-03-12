ece014e2bc21d586413d237af20dded33284765a
Viacheslav <viacheslavolshevskyi@gmail.com>
2018-03-12 21:09:49 +0200

    Menu list

--- angular/js/teacher/controllers/adminCmsCtrl.js
+++ angular/js/teacher/controllers/adminCmsCtrl.js
@@ -1,7 +1,7 @@
 angular
     .module('cmsApp')
-    .controller('cmsMenuListCtrl', ['$scope','cmsService',
-        function ($scope, cmsService) {
+    .controller('cmsMenuListCtrl', ['$scope','cmsService','$http',
+        function ($scope, cmsService,$http) {
             $scope.changePageHeader('Menu list');
 
             $scope.loadCmsMenuList=function(){
@@ -14,21 +14,39 @@ angular
             };
             $scope.loadCmsMenuList();
 
-            $scope.updateMenuLink=function(link){
-                cmsService.updateMenuLink({data:angular.toJson(link)}).$promise
-                    .then(function successCallback() {
-                        $scope.loadCmsMenuList();
-                        $scope.newLink={id:null, description:null,link:null};
-                    }, function errorCallback(response) {
-                        bootbox.alert(response.data.reason);
+            $scope.updateMenuLink=function(link,index,previousImage){
+                var image = $jq('#logo').prop('files')[0];
+                var uploadImage = new FormData();
+                if(index!==undefined){
+                    var imageUpdateBlock = '#logoUpdate'+index;
+                    var imageUpdate = $jq(imageUpdateBlock).prop('files')[0];
+                    uploadImage.append("logo",imageUpdate);
+                    uploadImage.append("previousImage",previousImage);
+                }
+                else{
+                    uploadImage.append("logo",image);
+                }
+                $http.post(basePath+'/_teacher/_admin/cms/setLogo', uploadImage, {
+                    withCredentials: true,
+                    headers: {'Content-Type': undefined },
+                    transformRequest: angular.identity
+                })
+                    .success(function (response) {
+                        link.image = response;
+                        cmsService.updateMenuLink({data:angular.toJson(link)}).$promise
+                            .then(function successCallback() {
+                                $scope.loadCmsMenuList();
+                                $scope.newLink={id:null, description:null,link:null};
+                            }, function errorCallback(response) {
+                                bootbox.alert(response.data.reason);
+                            });
                     });
             };
-            $scope.removeMenuLink=function(id){
-                cmsService.removeMenuLink({id:id}).$promise
+            $scope.removeMenuLink=function(id,image){
+                cmsService.removeMenuLink({id:id,image:image}).$promise
                     .then(function successCallback() {
                         $scope.loadCmsMenuList();
                     }, function errorCallback(response) {
-                        console.log(response);
                         bootbox.alert(response.data.reason);
                     });
             };
