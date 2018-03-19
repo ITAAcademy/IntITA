5b8645d9e1781906e7dacbf62e2c4e6ba1238f7a
Liubov Z <kirlyubov@gmail.com>
2018-03-19 18:39:51 +0200

    cms_news with images

--- protected/modules/_teacher/views/_admin/cms/_news.php
+++ protected/modules/_teacher/views/_admin/cms/_news.php
@@ -1,6 +1,5 @@
 <h1>News</h1>
 <div class="container">
-
     <div class="container text-center">
             <h1 class="text-muted">Как проходит обучение?</h1>
             <h3 class="text-primary info_bot">Пять шагов к исполнению твоих желаний</h3>
@@ -31,183 +30,32 @@
                     </div>
             </div>
         </div>
-
-
-
-
-
     </div>
 
-
-<div class="container">
-    <h3 class="text-right">Выбор курса или модуля</h3>
-    <p class="text-right">Дата публікації</p>
-    <div class="row">
-        <div>
-            <img src="https://intita.com/images/mainpage/step2.jpg" class="rightimg">
-        </div>
-        <div class="text-right">
-            Чтобы стать специалистом определенного направления и уровня (получить профессиональную специализацию) выбери для прохождения соответствующий курс. Если Тебя интересует исключительно углубление знаний в определенном направлении информационных технологий, то выбери интересующий Тебя модуль для прохождения.
-            <a href="#news2<? echo $index1 ?>" data-toggle="collapse" class="accordion-toggle collapsed">Детальніше</a>
-            <div id="news2<? echo $index1 ?>" class="collapse spoiler-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти б  Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
-                Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия.
+    <div class="container">
+        <h3 class="text-right">Выбор курса или модуля</h3>
+        <p class="text-right">Дата публікації</p>
+        <div class="row">
+            <div>
+                <img src="https://intita.com/images/mainpage/step2.jpg" class="rightimg">
+            </div>
+            <div class="text-right">
+                Чтобы стать специалистом определенного направления и уровня (получить профессиональную специализацию) выбери для прохождения соответствующий курс. Если Тебя интересует исключительно углубление знаний в определенном направлении информационных технологий, то выбери интересующий Тебя модуль для прохождения.
+                <a href="#news2<? echo $index1 ?>" data-toggle="collapse" class="accordion-toggle collapsed">Детальніше</a>
+                <div id="news2<? echo $index1 ?>" class="collapse spoiler-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти б  Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.
+                    Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия.
+                </div>
             </div>
         </div>
     </div>
 </div>
-</div>
-
-
-<!--    <div class="container">-->
-<!---->
-<!---->
-<!--        <div class="media">-->
-<!--            <div class="row">-->
-<!--                <div class="media-left media-middle">-->
-<!--                    <img src="https://intita.com/images/mainpage/step1.jpg" class="media-object">-->
-<!--                </div>-->
-<!--                <div class="media-body">-->
-<!--                    <h3 class="media-heading">Регистрация на сайте</h3>-->
-<!--                    <p>Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        <div class="media">-->
-<!--            <div class="media-body">-->
-<!--                <div class="text-right">-->
-<!--                <h3 class="media-heading">Выбор курса или модуля</h3>-->
-<!--                <p>Чтобы стать специалистом определенного направления и уровня (получить профессиональную специализацию) выбери для прохождения соответствующий курс. Если Тебя интересует исключительно углубление знаний в определенном направлении информационных технологий, то выбери интересующий Тебя модуль для прохождения.</p>-->
-<!--                </div>-->
-<!--                </div>-->
-<!--            <div class="media-right media-middle">-->
-<!--                <img src="https://intita.com/images/mainpage/step2.jpg" class="media-object">-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        <div class="media">-->
-<!--            <div class="media-left media-middle">-->
-<!--                <img src="https://intita.com/images/mainpage/step3.jpg" class="media-object">-->
-<!--            </div>-->
-<!--            <div class="media-body">-->
-<!--                <h3 class="media-heading">Оплата</h3>-->
-<!--                <p>Чтобы начать прохождение курса модуля, выбери схему оплаты (вся сумма за курс, оплата модулей, оплата потриместрово, помесячно и т.д.) и осуществи оплату удобным для Тебя способом (в процессе обучения есть возможность изменить схему оплаты курса или модуля, так же мы предоставляем возможность помесячной оплаты в кредит).</p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        <div class="media">-->
-<!--            <div class="media-body">-->
-<!--                <div class="text-right">-->
-<!--                    <h3 class="media-heading">Изучение материала</h3>-->
-<!--                    <p>Изучать материал возможно путем чтения текста и/или просмотра видео для каждого занятия. Во время прохождения занятия решай Промежуточные тестовые задания. По окончанию каждого занятия проходи Финальное тестовое задание. Каждый модуль завершается Индивидуальным проектом или Экзаменом. Можно получить индивидуальную консультацию преподавателя или консультацию онлайн.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="media-right media-middle">-->
-<!--                <img src="https://intita.com/images/mainpage/step4.jpg" class="media-object">-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        <div class="media">-->
-<!--            <div class="media-left media-middle">-->
-<!--                <img src="https://intita.com/images/mainpage/step5.jpg" class="media-object">-->
-<!--            </div>-->
-<!--            <div class="media-body">-->
-<!--                <h3 class="media-heading">Завершение курса</h3>-->
-<!--                <p>Курс оканчивается Командным дипломным проектом, который готовится вместе с другими студентами (состав команды формируешь самостоятельно или рекомендует тренер, который утверждает тему и техническое задание проекта). Сдача проекта предусматривает предзащиту и защиту в онлайн-режиме с представлением технической документации. После защиты выдается диплом и рекомендация для трудоустройства.</p>-->
-<!--            </div>-->
-<!--        </div>-->
-
-<!--        <div class="items">-->
-<!--            <div class="stepLeft">-->
-<!--                <div class="stepUrl">-->
-<!--                    <img class="stepImg" src="https://intita.com/images/mainpage/step1.jpg">-->
-<!--                </div>-->
-<!--                <div class="stepInfo" style="min-height:220px">-->
-<!--                    <h2>Регистрация на сайте</h2>-->
-<!--                    <p>Зарегистрируйся на сайте, чтобы получить доступ к курсам и пройти бесплатные модули и занятия. Это позволит Тебе оценить качество и удобство нашего продукта, который станет для Тебя надежным партнером и советчиком в профессиональной самореализации.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-
-<!--            <div class="stepRight">-->
-<!--                <div class="stepUrl">-->
-<!--                    <img class="stepImg" src="https://intita.com/images/mainpage/step2.jpg">-->
-<!--                </div>-->
-<!--                <div class="stepInfo">-->
-<!--                    <h2>Выбор курса или модуля</h2>-->
-<!--                    <p>Чтобы стать специалистом определенного направления и уровня (получить профессиональную специализацию) выбери для прохождения соответствующий курс. Если Тебя интересует исключительно углубление знаний в определенном направлении информационных технологий, то выбери интересующий Тебя модуль для прохождения.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-
-
-
-<!--            <div class="stepLeft">-->
-<!--                <div class="stepUrl">-->
-<!--                    <img class="stepImg" src="https://intita.com/images/mainpage/step3.jpg">-->
-<!--                </div>-->
-<!--                <div class="stepInfo" style="min-height:220px">-->
-<!--                    <h2>Оплата</h2>-->
-<!--                    <p>Чтобы начать прохождение курса модуля, выбери схему оплаты (вся сумма за курс, оплата модулей, оплата потриместрово, помесячно и т.д.) и осуществи оплату удобным для Тебя способом (в процессе обучения есть возможность изменить схему оплаты курса или модуля, так же мы предоставляем возможность помесячной оплаты в кредит).</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="stepRight">-->
-<!--                <div class="stepUrl">-->
-<!--                    <img class="stepImg" src="https://intita.com/images/mainpage/step4.jpg">-->
-<!--                </div>-->
-<!--                <div class="stepInfo">-->
-<!--                    <h2>Изучение материала</h2>-->
-<!--                    <p>Изучать материал возможно путем чтения текста и/или просмотра видео для каждого занятия. Во время прохождения занятия решай Промежуточные тестовые задания. По окончанию каждого занятия проходи Финальное тестовое задание. Каждый модуль завершается Индивидуальным проектом или Экзаменом. Можно получить индивидуальную консультацию преподавателя или консультацию онлайн.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="stepLeft">-->
-<!--                <div class="stepUrl">-->
-<!--                    <img class="stepImg" src="https://intita.com/images/mainpage/step5.jpg">-->
-<!--                </div>-->
-<!--                <div class="stepInfo" style="min-height:220px">-->
-<!--                    <h2>Завершение курса</h2>-->
-<!--                    <p>Курс оканчивается Командным дипломным проектом, который готовится вместе с другими студентами (состав команды формируешь самостоятельно или рекомендует тренер, который утверждает тему и техническое задание проекта). Сдача проекта предусматривает предзащиту и защиту в онлайн-режиме с представлением технической документации. После защиты выдается диплом и рекомендация для трудоустройства.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="keys" style="display:none" title="/"><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span></div>-->
-<!--    </div>-->
-
-<!---->
-<!---->
-<!--    <div class="bannersArea">-->
-<!--        <div class="bannersCarousel ng-scope" ng-controller="bannersSliderCtrl">-->
-<!--            <ul rn-carousel="" rn-carousel-index="1" rn-carousel-auto-slide="" rn-carousel-pause-on-hover="" rn-carousel-buffered="" rn-carousel-transition="hexagon" rn-carousel-duration="4" class="ng-scope">-->
-<!--                <!-- ngRepeat: slide in slides|carouselSlice:carouselBufferIndex:carouselBufferSize track by slide.id --><li ng-repeat="slide in slides|carouselSlice:carouselBufferIndex:carouselBufferSize track by slide.id" ng-class="'id-' + slide.id" class="ng-scope id-2" style="display: inline-block; transform: translate3d(-200%, 0px, 0px) rotateY(-120deg); transform-origin: 100% 50% 0px;">-->
-<!--                    <a href="http://hack.intita.com" target="_blank">-->
-<!--                        <div ng-style="{'background-image': 'url(' + slide.file_path + ')','background-size':'contain', 'background-repeat':'no-repeat' }" class="bannerImage" style="background-image: url(&quot;/images/banners/hackers.gif&quot;); background-size: contain; background-repeat: no-repeat;">-->
-<!--                            &nbsp;-->
-<!--                        </div>-->
-<!--                    </a>-->
-<!--                </li><!-- end ngRepeat: slide in slides|carouselSlice:carouselBufferIndex:carouselBufferSize track by slide.id --><li ng-repeat="slide in slides|carouselSlice:carouselBufferIndex:carouselBufferSize track by slide.id" ng-class="'id-' + slide.id" class="ng-scope id-3" style="display: inline-block; transform: translate3d(-100%, 0px, 0px) rotateY(-60deg); transform-origin: 100% 50% 0px;">-->
-<!--                    <a href="https://kavadrive.com" target="_blank">-->
-<!--                        <div ng-style="{'background-image': 'url(' + slide.file_path + ')','background-size':'contain', 'background-repeat':'no-repeat' }" class="bannerImage" style="background-image: url(&quot;/images/banners/kavadrive.gif&quot;); background-size: contain; background-repeat: no-repeat;">-->
-<!--                            &nbsp;-->
-<!--                        </div>-->
-<!--                    </a>-->
-<!--                </li><!-- end ngRepeat: slide in slides|carouselSlice:carouselBufferIndex:carouselBufferSize track by slide.id --><li ng-repeat="slide in slides|carouselSlice:carouselBufferIndex:carouselBufferSize track by slide.id" ng-class="'id-' + slide.id" class="ng-scope id-4" style="display: inline-block; transform: translate3d(0%, 0px, 0px) rotateY(0deg); transform-origin: 0% 50% 0px;">-->
-<!--                    <a href="https://docs.google.com/forms/d/e/1FAIpQLScDQYWVmSFaAxD4_fGV3aKOIRpF9j-UtD0UXQVG5WMECRM8rg/viewform" target="_blank">-->
-<!--                        <div ng-style="{'background-image': 'url(' + slide.file_path + ')','background-size':'contain', 'background-repeat':'no-repeat' }" class="bannerImage" style="background-image: url(&quot;/images/banners/deep-drive.gif&quot;); background-size: contain; background-repeat: no-repeat;">-->
-<!--                            &nbsp;-->
-<!--                        </div>-->
-<!--                    </a>-->
-<!--                </li><!-- end ngRepeat: slide in slides|carouselSlice:carouselBufferIndex:carouselBufferSize track by slide.id -->-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    </div>-->
-
-<!--</div>-->
