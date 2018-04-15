5fdd0d2efb9fcc4eb4df9a0f8d00c8049c8c1427
Wizlight <wizlightdragon@gmail.com>
2018-04-15 18:59:43 +0300

    subdomain validation

--- protected/modules/_teacher/views/_admin/cms/subdomain.php
+++ protected/modules/_teacher/views/_admin/cms/subdomain.php
@@ -1,9 +1,16 @@
 <div class="panel panel-default" ng-controller="subdomainCtrl">
-    <button class="btn btn-success" ng-if="subdomainsTableData.total()==0"
-            ng-bootbox-prompt="Додати субдомен"
-            ng-bootbox-prompt-action="addSubdomain(result)">
-        Додати субдомен
-    </button>
+    <a href="" ng-show="subdomainsTableData.total()==0" ng-click="showForm=!showForm">Створити субдомен</a>
+    <form name='subdomainForm' ng-if="showForm && subdomainsTableData.total()==0">
+        <label>Назва піддомену*</label>
+        <input class="form-control" type="text" ng-required="true" ng-pattern="/^[a-zA-Z0-9_]+$/"  ng-minlength="1" ng-maxlength="15" placeholder="Небільше 15 латинських символів, цифр або спеціальних символів -,_"
+               ng-model="name">
+        Посилання на сайт буде мати вигляд: https://{{name}}.intita.com
+        <div ng-cloak class="clientValidationError" ng-show="subdomainForm.$invalid">
+            Введіть коректну назву (Небільше 15 латинських символів, цифр або спеціальних символів -,_)
+        </div>
+        <br>
+        <button class="btn btn-primary" ng-click="addSubdomain(name)" ng-disabled="subdomainForm.$invalid">Додати субдомен</button>
+    </form>
     <div class="panel-body">
         <div class="dataTable_wrapper">
             <table ng-table="subdomainsTableData" class="table table-striped table-bordered table-hover"
