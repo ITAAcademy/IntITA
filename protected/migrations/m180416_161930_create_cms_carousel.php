7a019957464855f463851b0487a8d68958230042
Andrii Polishchuk <22andrypoli@gmail.com>
2018-04-23 17:12:55 +0300

    migration repairing

--- protected/migrations/m180416_161930_create_cms_carousel.php
+++ protected/migrations/m180416_161930_create_cms_carousel.php
@@ -6,19 +6,18 @@ class m180416_161930_create_cms_carousel extends CDbMigration
 	{
         $this->createTable('cms_carousel', array(
             'id' => 'INT UNSIGNED PRIMARY KEY AUTO_INCREMENT',
-            'id_organization' => 'SMALLINT UNSIGNED NOT NULL',
+            'id_organization' => 'INT(11) NOT NULL',
             'position' => 'MEDIUMINT UNSIGNED NOT NULL',
-            'src' => 'CHAR(100) NOT NULL',
-            'title' => 'TEXT NOT NULL DEFAULT \'\'',
+            'src' => 'VARCHAR(256) NOT NULL',
+            'title' => 'VARCHAR(256) DEFAULT NULL',
+            'description' => 'TEXT DEFAULT NULL',
         ));
-        $this->createIndex('org_index', 'cms_carousel', 'id_organization', false);
         $this->addForeignKey('FK_cms_carousel_organization','cms_carousel','id_organization','organization','id', 'RESTRICT', 'RESTRICT');
 	}
 
 	public function down()
 	{
         $this->dropForeignKey('FK_cms_carousel_organization','cms_carousel');
-        $this->dropIndex('org_index', 'cms_carousel');
         $this->dropTable('cms_carousel');
 	}
 }
