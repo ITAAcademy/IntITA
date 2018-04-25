c93269847ceb878943214c28319fb5383eab1a36
олег ростов <terabyte.or@gmail.com>
2018-04-25 10:34:56 +0300

    merge origin master

---
+++ protected/controllers/ForPartnersController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Viacheslav
+ * Date: 3/30/2018
+ * Time: 1:19 PM
+ */
+class ForPartnersController extends Controller
+{
+    public function actionIndex(){
+        $this->render('index',array());
+    }
+}
