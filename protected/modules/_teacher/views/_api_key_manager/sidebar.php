a16f4b2e58062479d6c0c4f6a891e159abb61992
Viacheslav <viacheslavolshevskyi@gmail.com>
2018-05-11 13:25:25 +0300

    add api key manager in sidebar

---
+++ protected/modules/_teacher/views/_api_key_manager/sidebar.php
@@ -0,0 +1,8 @@
+<li>
+    <a href="#/#" class="show_elem">
+        <i class="fa fa-key fa-fw"></i> API Key Manager
+    </a>
+    <a href="#/#" uib-tooltip="API Key Manager" tooltip-placement="right" class="hid" style="display: none">
+        <i class="fa fa-key fa-fw"></i>
+    </a>
+</li>
