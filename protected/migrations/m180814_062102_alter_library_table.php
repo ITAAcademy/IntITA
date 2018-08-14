8eadffb58d5c49a85abdab7ae6c1820f3a338d07
олег ростов <terabyte.or@gmail.com>
2018-08-14 10:41:59 +0300

    migration for library

---
+++ protected/migrations/m180814_062102_alter_library_table.php
@@ -0,0 +1,19 @@
+<?php
+
+class m180814_062102_alter_library_table extends CDbMigration
+{
+	public function safeUp()
+	{
+		$this->addColumn('library', 'publication_year', 'INT DEFAULT NULL');
+		$this->addColumn('library', 'position', 'INT NOT NULL');
+		foreach (Library::model()->findAll() as $lib){
+            $this->update('library', array('position' => $lib->id), 'id='.$lib->id);
+        }
+	}
+
+	public function safeDown()
+	{
+		$this->dropColumn('library', 'position');
+        $this->dropColumn('library', 'publication_year');
+	}
+}
