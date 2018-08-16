cab9bc2e2451231a13e918e87221e8786d839297
олег ростов <terabyte.or@gmail.com>
2018-08-16 17:15:40 +0300

    changed logo img from png to svg

--- protected/views/site/_shareMetaTagMain.php
+++ protected/views/site/_shareMetaTagMain.php
@@ -1,6 +1,6 @@
 <div style="display: none">
     <h1 itemprop="name"><?php echo $title ?></h1>
-    <img itemprop="image" src="<?php echo StaticFilesHelper::createPath('image', 'mainpage', 'intitaLogo.jpg'); ?>"/>
+    <img itemprop="image" src="<?php echo StaticFilesHelper::createPath('image', 'mainpage', 'hamburgerlogo.svg'); ?>"/>
     <p itemprop="description"><?php echo $description ?></p>
 </div>
 <?php
@@ -16,7 +16,7 @@ Yii::app()->clientScript->registerMetaTag($description, null, null, array('name'
 <!--    <div class="share42init" data-top1="110" data-top2="70" data-margin="15"-->
 <!--         data-url="--><?php //echo $url ?><!--"-->
 <!--         data-title="--><?php //echo $title ?><!--"-->
-<!--         data-image="--><?php //echo StaticFilesHelper::createPath('image', 'mainpage', 'intitaLogo.jpg') ?><!--"-->
+<!--         data-image="--><?php //echo StaticFilesHelper::createPath('image', 'mainpage', 'hamburgerlogo.svg') ?><!--"-->
 <!--         data-description="--><?php //echo $description ?><!--"-->
 <!--         data-path="--><?php //echo Config::getBaseUrl(); ?><!--/scripts/share42/"-->
 <!--         data-zero-counter="1">-->
@@ -26,7 +26,7 @@ Yii::app()->clientScript->registerMetaTag($description, null, null, array('name'
     <div class="share42init"  data-top1="110" data-top2="70" data-margin="15"
          data-url="<?php echo $url ?>"
          data-title="<?php echo $title ?>"
-         data-image="<?php echo StaticFilesHelper::createPath('image', 'mainpage', 'intitaLogo.jpg') ?>"
+         data-image="<?php echo StaticFilesHelper::createPath('image', 'mainpage', 'hamburgerlogo.svg') ?>"
          data-description="<?php echo $description ?>"
          data-path="<?php echo Config::getBaseUrl(); ?>/scripts/share42/"
          data-icons-file="icons.png"
@@ -37,7 +37,7 @@ Yii::app()->clientScript->registerMetaTag($description, null, null, array('name'
     <div class="share42init"  data-top1="110" data-top2="70" data-margin="22"
          data-url="<?php echo $url ?>"
          data-title="<?php echo $title ?>"
-         data-image="<?php echo StaticFilesHelper::createPath('image', 'mainpage', 'intitaLogo.jpg') ?>"
+         data-image="<?php echo StaticFilesHelper::createPath('image', 'mainpage', 'hamburgerlogo.svg') ?>"
          data-description="<?php echo $description ?>"
          data-path="<?php echo Config::getBaseUrl(); ?>/scripts/share42/"
          data-icons-file="little_icons.png"
