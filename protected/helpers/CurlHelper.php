1b44d055d3e871271510ac229eda1ff99388768a
dell <Wizlightdragon@gmail.com>
2018-12-15 15:12:07 +0200

    upload

--- protected/helpers/CurlHelper.php
+++ protected/helpers/CurlHelper.php
@@ -39,12 +39,60 @@ class CurlHelper
 
     function loadImageToDependServer($url, $filename, $file, $path = null)
     {
+
+        $target_url = $url;
+        //This needs to be the full path to the file you want to send.
+        $file_name_with_full_path =$file['tmp_name']['course_img'];
+        /* curl will accept an array here too.
+         * Many examples I found showed a url-encoded string instead.
+         * Take note that the 'key' in the array will be the key that shows up in the
+         * $_FILES array of the accept script. and the at sign '@' is required before the
+         * file name.
+         */
+        $post = array('extra_info' => '123456','file_contents'=>'@'.$file_name_with_full_path);
         $ch = curl_init();
-        $data = array('name' => $filename, 'file' => $file, 'path' => $path);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_exec($ch);
+        curl_setopt($ch, CURLOPT_URL,$target_url);
+        curl_setopt($ch, CURLOPT_POST,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        $result=curl_exec ($ch);
+        curl_close ($ch);
+        echo $result;
+
+
+//
+//        $tmpfile = $file['tmp_name']['course_img'];
+//
+////        $data = array(
+////            'uploaded_file' => '@'.$tmpfile.';filename='.$filename,
+////        );
+//
+//        $data = array(
+//            'file' => '@'.$tmpfile.';filename='.$filename,
+//        );
+//        $ch = curl_init();
+////        $data = array('name' => $filename, 'file' => '@'.$tmpfile, 'path' => $path);
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//        curl_exec($ch);
+//
+
+//        $tmpfile = $file['tmp_name']['course_img'];
+//        $filename = basename($file['name']['course_img']);
+//
+//        $data = array(
+//            'uploaded_file' => '@'.$tmpfile.';filename='.$filename,
+//        );
+//
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//// set your other cURL options here (url, etc.)
+//
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+//
+//        curl_exec($ch);
     }
 
 }
