50a9c0b4943d0d99d7d822cb5099120b35923f6a
dell <Wizlightdragon@gmail.com>
2019-01-05 14:13:03 +0200

    upload files from new server

--- protected/components/HttpRequest.php
+++ protected/components/HttpRequest.php
@@ -41,18 +41,9 @@ class HttpRequest extends CHttpRequest  {
             $session=Yii::app()->session;
 
             $route = Yii::app()->getUrlManager()->parseUrl($this);
-            var_dump('1111');
-            var_dump($route);
-            var_dump('2222');
-            var_dump($this->noCsrfValidationRoutes);
-            var_dump('3333');
-            var_dump(in_array($route, $this->noCsrfValidationRoutes));
-            var_dump('44444');
-            var_dump(array_search($route, $this->noCsrfValidationRoutes));
-            var_dump('5555');
-            var_dump(false!==array_search($route, $this->noCsrfValidationRoutes));
-            var_dump('6666');die;
-            if(($session->contains($this->csrfTokenName) && $isTokenInRequest) || in_array($route, $this->noCsrfValidationRoutes))
+            if(in_array($route, $this->noCsrfValidationRoutes)){
+                $valid = true;
+            } else if(($session->contains($this->csrfTokenName) && $isTokenInRequest))
             {
                 $tokenFromSession=$session->itemAt($this->csrfTokenName);
                 $tokenFromRequest=Yii::app()->request->getCsrfToken();
