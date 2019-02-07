8108ad585b97e3505db95bda0b19f878b333e166
dell <Wizlightdragon@gmail.com>
2019-02-07 18:13:39 +0200

    liqpay fix

--- protected/models/LiqpayPayment.php
+++ protected/models/LiqpayPayment.php
@@ -138,4 +138,24 @@ class LiqpayPayment extends CActiveRecord
             $o[] = $decrypt ? $s1[$al[1]][$al[0]] : $s2[$al[1]][$al[0]];
         return implode('',$o);
     }
+
+    static function dsCryptOrder($string,$decrypt=false) {
+        // you may change these values to your own
+        $secret_key = 'my_simple_secret_key';
+        $secret_iv = 'my_simple_secret_iv';
+
+        $output = false;
+        $encrypt_method = "AES-256-CBC";
+        $key = hash( 'sha256', $secret_key );
+        $iv = substr( hash( 'sha256', $secret_iv ), 0, 16 );
+
+        if( !$decrypt ) {
+            $output = base64_encode( openssl_encrypt( $string, $encrypt_method, $key, 0, $iv ) );
+        }
+        else if( $decrypt ){
+            $output = openssl_decrypt( base64_decode( $string ), $encrypt_method, $key, 0, $iv );
+        }
+
+        return $output;
+    }
 }
